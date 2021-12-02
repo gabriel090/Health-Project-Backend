78b8193ddd07785e017d58f2a2aa9e3e7f12d25f
unknown <gmbatha@mhealthkenya.org>
2021-12-02 20:16:59 +0300

    first commit

---
+++ src/main/java/com/health/project/Health/Project/HealthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.health.project.Health.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/health/project/Health/Project/HealthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.project.Health.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
